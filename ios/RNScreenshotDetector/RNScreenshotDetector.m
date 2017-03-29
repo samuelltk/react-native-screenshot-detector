65148c093129d3ad0dd7b3c2eafa6843093efcf4
samuelltk <luuyangg@126.com>
2017-03-29 14:35:08 +0800

    Update RNScreenshotDetector.m

--- ios/RNScreenshotDetector/RNScreenshotDetector.m
+++ ios/RNScreenshotDetector/RNScreenshotDetector.m
@@ -31,7 +31,12 @@ RCT_EXPORT_MODULE();
                                                       object:nil
                                                        queue:mainQueue
                                                   usingBlock:^(NSNotification *notification) {
-                                                      [self screenshotDetected:notification];
+                                                      double delayInSeconds = 1.0;
+                                                      __block RCTEventEmitter* bself = self;
+                                                      dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+                                                      dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+                                                         [self screenshotDetected:notification];
+                                                      });
                                                   }];
 }
 
