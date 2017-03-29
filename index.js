b05df8a54a1e774ee22be634d35159edb63d7fde
samuelltk <luuyangg@126.com>
2017-03-29 16:11:53 +0800

    Update index.js
    add will detect screen shot

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ import { NativeEventEmitter, NativeModules } from 'react-native';
 const { RNScreenshotDetector } = NativeModules;
 
 export const SCREENSHOT_EVENT = 'ScreenshotTaken';
+export const WILL_SCREENSHOT_EVENT = 'ScreenshotWillTaken';
 
-export function subscribe(cb) {
+export function subscribe(willcb,cb) {
   const eventEmitter = new NativeEventEmitter(RNScreenshotDetector);
   eventEmitter.addListener(SCREENSHOT_EVENT, cb, {});
+  eventEmitter.addListener(WILL_SCREENSHOT_EVENT, willcb, {});
   return eventEmitter;
 }
 
 export function unsubscribe(eventEmitter) {
   eventEmitter.removeAllListeners(SCREENSHOT_EVENT);
+  eventEmitter.removeAllListeners(WILL_SCREENSHOT_EVENT);
 }
 
 export function saveImage(obj, successCallBack, errorCallback)
