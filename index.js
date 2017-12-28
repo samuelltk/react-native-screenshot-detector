7e496eda47d3fd8c64f47af5e5cf9f2010da1c08
samuelltk <luuyangg@126.com>
2017-12-28 16:55:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-
 import { NativeEventEmitter, NativeModules } from 'react-native';
 const { RNScreenshotDetector } = NativeModules;
 
 export const SCREENSHOT_EVENT = 'ScreenshotTaken';
-export const WILL_SCREENSHOT_EVENT = 'ScreenshotWillTaken';
 
-export function subscribe(willcb,cb) {
+export function subscribe(cb) {
   const eventEmitter = new NativeEventEmitter(RNScreenshotDetector);
   eventEmitter.addListener(SCREENSHOT_EVENT, cb, {});
-  eventEmitter.addListener(WILL_SCREENSHOT_EVENT, willcb, {});
   return eventEmitter;
 }
 
 export function unsubscribe(eventEmitter) {
   eventEmitter.removeAllListeners(SCREENSHOT_EVENT);
-  eventEmitter.removeAllListeners(WILL_SCREENSHOT_EVENT);
 }
 
 export function saveImage(obj, successCallBack, errorCallback)
